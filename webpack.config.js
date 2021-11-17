492f70c5c6f3326d7a3c60e4bfebe86b8dde3cf6
Matt Tedder <mjtedder@icloud.com>
2021-11-16 17:08:16 -0700

    Adds config file for webpack.

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    "mode": "development",
+    "entry": "./src/index.js",
+    "output": {
+        "path": __dirname + '/dir',
+        "filename": "bundle.js"
+    },
+    "target": ["web", "es5"],
+    "devtool": "source-map",
+    "module": {
+        "rules": [
+            {
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader"
+                }
+            },
+            {
+                "test": /\.css$/,
+                "use": [
+                    "style-loader",
+                    "css-loader"
+                ]
+            },
+            {
+                "test": /\.html$/,
+                "use": [
+                    {
+                        "loader": "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    "plugins": [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+}
